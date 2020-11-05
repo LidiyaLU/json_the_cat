a2b728ad9110919ae12309d80db546c5d57f1d6c
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-05 01:18:15 +0000

    improve catBreed via adding modules

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,18 @@
 const request = require('request');
+
+let cat = process.argv[2];
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + cat, function (error, response, body) {
+
+  if (error) {
+    console.log(error);
+    console.log(responce.statusCode);
+   }
+
+  const data = JSON.parse(body);
+  if (data.length > 0) {
+  console.log(data[0].description);
+  } else {
+    console.log('there is no such breed');
+  }
+  });
