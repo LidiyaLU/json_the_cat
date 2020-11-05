11f39547589d5380de6bfb1af8c5a378c2e1e1a2
LidiyaLU <cheerrlidiya@yandex.ru>
2020-11-05 01:20:19 +0000

    add index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,17 +2,26 @@ const request = require('request');
 
 let cat = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + cat, function (error, response, body) {
+const fetchBreedDescription = function(breedName, callback) {
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+
+request(url, function (error, response, body) {
 
   if (error) {
     console.log(error);
     console.log(responce.statusCode);
+    return callback(error, null);
    }
 
   const data = JSON.parse(body);
-  if (data.length > 0) {
-  console.log(data[0].description);
-  } else {
-    console.log('there is no such breed');
-  }
-  });
+  if (!data.length) {
+    return callback('cant find breed', null);
+  } 
+
+  const breed = data[0];
+
+  return callback(null, breed.description);
+  })
+};
+
+module.exports = {fetchBreedDescription};
